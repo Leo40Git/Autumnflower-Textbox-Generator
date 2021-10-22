48c16da87c1c8ccb4da5f58d1137f58601acbeae
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-10-22 21:45:08 +0300

    WindowBorder: Accuracy fixes
    Now it should work with all Window.png sheets!

--- src/main/java/adudecalledleo/aftbg/window/WindowBorder.java
+++ src/main/java/adudecalledleo/aftbg/window/WindowBorder.java
@@ -6,7 +6,8 @@ import java.awt.image.ImageObserver;
 
 public final class WindowBorder {
     // in essence, this is a modified "nine patch" renderer
-    // the center/middle pieces are thinner: 32x24 for center-left/right pieces and 24x32 for top/bottom-middle pieces
+    // corner pieces are 24x24
+    // the center/middle pieces are longer: 24x48 for center-left/right pieces and 48x24 for top/bottom-middle pieces
     // (RPG Maker puts some menu-related arrows here, for some reason)
     // also, a center-middle piece doesn't exist, so technically this is an "eight patch" renderer
 
@@ -20,37 +21,38 @@ public final class WindowBorder {
     private static final int PIECE_BR = 7;
     private static final int PIECE_MAX = 8;
 
-    private final int pieceWidth = 32, pieceHeight = 32;
-    private final int pieceWidthC = 24, pieceHeightM = 24;
+    private final int pieceWidth = 24, pieceHeight = 24;
+    private final int pieceWidthM = 48, pieceHeightC = 48;
 
     private final BufferedImage[] pieces = new BufferedImage[PIECE_MAX];
 
     public WindowBorder(BufferedImage window) {
         final int startX = 96;
+        final int middlePadding = 48; // padding between PIECE_CL's end and PIECE_CR's start
 
         pieces[PIECE_TL] = window.getSubimage(startX, 0, pieceWidth, pieceHeight);
-        pieces[PIECE_TM] = window.getSubimage(startX + pieceWidth, 0, pieceWidth, pieceHeightM);
-        pieces[PIECE_TR] = window.getSubimage(startX + pieceWidth * 2, 0, pieceWidth, pieceHeight);
+        pieces[PIECE_TM] = window.getSubimage(startX + pieceWidth, 0, pieceWidthM, pieceHeight);
+        pieces[PIECE_TR] = window.getSubimage(startX + pieceWidth + pieceWidthM, 0, pieceWidth, pieceHeight);
 
-        pieces[PIECE_CL] = window.getSubimage(startX, pieceHeight, pieceWidthC, pieceHeight);
-        pieces[PIECE_CR] = window.getSubimage(startX + pieceWidth * 2 + (pieceWidth - pieceWidthC), pieceHeight, pieceWidthC, pieceHeight);
+        pieces[PIECE_CL] = window.getSubimage(startX, pieceHeight, pieceWidth, pieceHeightC);
+        pieces[PIECE_CR] = window.getSubimage(startX + pieceWidth + middlePadding, pieceHeight, pieceWidth, pieceHeightC);
 
-        pieces[PIECE_BL] = window.getSubimage(startX, pieceHeight * 2, pieceWidth, pieceHeight);
-        pieces[PIECE_BM] = window.getSubimage(startX + pieceWidth, pieceHeight * 2 + (pieceHeight - pieceHeightM), pieceWidth, pieceHeightM);
-        pieces[PIECE_BR] = window.getSubimage(startX + pieceWidth * 2, pieceHeight * 2, pieceWidth, pieceHeight);
+        pieces[PIECE_BL] = window.getSubimage(startX, pieceHeight + pieceHeightC, pieceWidth, pieceHeight);
+        pieces[PIECE_BM] = window.getSubimage(startX + pieceWidth, pieceHeight + pieceHeightC, pieceWidthM, pieceHeight);
+        pieces[PIECE_BR] = window.getSubimage(startX + pieceWidth + pieceWidthM, pieceHeight + pieceHeightC, pieceWidth, pieceHeight);
     }
 
     public void draw(Graphics g, int x, int y, int width, int height, ImageObserver observer) {
         // TOP
         g.drawImage(pieces[PIECE_TL], x, y, observer);
-        g.drawImage(pieces[PIECE_TM], x + pieceWidth, y, width - pieceWidth * 2, pieceHeightM, observer);
+        g.drawImage(pieces[PIECE_TM], x + pieceWidth, y, width - pieceWidthM, pieceHeight, observer);
         g.drawImage(pieces[PIECE_TR], x + width - pieceWidth, y, observer);
         // CENTER
-        g.drawImage(pieces[PIECE_CL], x, y + pieceHeight, pieceWidthC, height - pieceHeight * 2, observer);
-        g.drawImage(pieces[PIECE_CR], x + width - pieceWidthC, y + pieceHeight, pieceWidthC, height - pieceHeight * 2, observer);
+        g.drawImage(pieces[PIECE_CL], x, y + pieceHeight, pieceWidth, height - pieceHeightC, observer);
+        g.drawImage(pieces[PIECE_CR], x + width - pieceWidth, y + pieceHeight, pieceWidth, height - pieceHeightC, observer);
         // BOTTOM
         g.drawImage(pieces[PIECE_BL], x, y + height - pieceHeight, observer);
-        g.drawImage(pieces[PIECE_BM], x + pieceWidth, y + height - pieceHeightM, width - pieceWidth * 2, pieceHeightM, observer);
+        g.drawImage(pieces[PIECE_BM], x + pieceWidth, y + height - pieceHeight, width - pieceWidthM, pieceHeight, observer);
         g.drawImage(pieces[PIECE_BR], x + width - pieceWidth, y + height - pieceHeight, observer);
     }
 }
