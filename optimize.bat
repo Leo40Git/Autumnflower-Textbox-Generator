562ecc0fcdbc3552752fc8741ca534949841bf7d
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-01-04 18:37:38 +0200

    Rewrite optimize script
    Remove random white pixel from face

--- optimize.bat
+++ optimize.bat
@@ -4,13 +4,20 @@ REM This script requires Oxipng to be on your PATH.
 REM Download it here: https://github.com/shssoichiro/oxipng/releases/latest
 
 setlocal
+REM Modify this variable to pass options to Oxipng
 set OXIPNG_OPTIONS=-a -s
-pushd scratch
-echo === Optimizing %cd% and subdirectories...
-oxipng %OXIPNG_OPTIONS% -r *
+
+pushd %~dp0
+call :optimize_dir scratch
+call :optimize_dir scratch_ex
 popd
-pushd scratch_ex
-echo === Optimizing %cd% and subdirectories...
-oxipng %OXIPNG_OPTIONS% -r *
-popd
+
 endlocal
+exit /b 0
+
+:optimize_dir dirname
+pushd %1
+echo === Optimizing %cd% and subdirectories...
+oxipng %OXIPNG_OPTIONS% -r *
+echo === Done with  %cd% and subdirectories
+popd
