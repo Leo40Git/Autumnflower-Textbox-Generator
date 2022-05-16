33ba107bc12a9a91faa59dc1e7e9a309a181d19b
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-05-16 12:42:52 +0300

    WindowBackground.BaseComposite: only sample the SOURCE, not the DESTINATION

--- src/main/java/adudecalledleo/aftbg/window/WindowBackground.java
+++ src/main/java/adudecalledleo/aftbg/window/WindowBackground.java
@@ -101,16 +101,15 @@ public final class WindowBackground {
                 int w = Math.min(src.getWidth(), dstIn.getWidth());
                 int h = Math.min(src.getHeight(), dstIn.getHeight());
 
-                int[] dstRgba = new int[4];
+                int[] srcRgba = new int[4];
 
                 for (int x = 0; x < w; x++) {
                     for (int y = 0; y < h; y++) {
-                        dstIn.getPixel(x + dstIn.getMinX(), y + dstIn.getMinY(), dstRgba);
-                        dstRgba[0] = Math.min(255, Math.max(0, dstRgba[0] + color.red()));
-                        dstRgba[1] = Math.min(255, Math.max(0, dstRgba[1] + color.green()));
-                        dstRgba[2] = Math.min(255, Math.max(0, dstRgba[2] + color.blue()));
-                        dstRgba[3] = 255;
-                        dstOut.setPixel(x + dstOut.getMinX(), y + dstOut.getMinY(), dstRgba);
+                        src.getPixel(x + src.getMinX(), y + src.getMinY(), srcRgba);
+                        srcRgba[0] = Math.min(255, Math.max(0, srcRgba[0] + color.red()));
+                        srcRgba[1] = Math.min(255, Math.max(0, srcRgba[1] + color.green()));
+                        srcRgba[2] = Math.min(255, Math.max(0, srcRgba[2] + color.blue()));
+                        dstOut.setPixel(x + dstOut.getMinX(), y + dstOut.getMinY(), srcRgba);
                     }
                 }
             }
