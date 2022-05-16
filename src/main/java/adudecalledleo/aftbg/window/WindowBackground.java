c3a090bcb02a324acafdf7d0c9898ffdc855cf11
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2022-05-16 12:38:05 +0300

    WindowBackgroundBaseComposite: make inner class of WindowBackground
    rename to BaseComposite

--- src/main/java/adudecalledleo/aftbg/window/WindowBackground.java
+++ src/main/java/adudecalledleo/aftbg/window/WindowBackground.java
@@ -48,7 +48,7 @@ public final class WindowBackground {
             Graphics2D sg = scratchBuf.createGraphics();
 
             // draw stretched and tinted base
-            sg.setComposite(new WindowBackgroundBaseComposite(color));
+            sg.setComposite(new BaseComposite(color));
             sg.drawImage(base, 0, 0, width, height, 0, 0, base.getWidth(), base.getHeight(), null);
             // draw tiled overlay
             sg.setComposite(AlphaComposite.SrcOver);
@@ -78,5 +78,42 @@ public final class WindowBackground {
             return true;
         }
         return false;
+    }
+
+    private static final class BaseComposite implements Composite {
+        private final Context context;
+
+        public BaseComposite(WindowTint color) {
+            context = new Context(color);
+        }
+
+        @Override
+        public CompositeContext createContext(ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints) {
+            return context;
+        }
+
+        private record Context(WindowTint color) implements CompositeContext {
+            @Override
+            public void dispose() { }
+
+            @Override
+            public void compose(Raster src, Raster dstIn, WritableRaster dstOut) {
+                int w = Math.min(src.getWidth(), dstIn.getWidth());
+                int h = Math.min(src.getHeight(), dstIn.getHeight());
+
+                int[] dstRgba = new int[4];
+
+                for (int x = 0; x < w; x++) {
+                    for (int y = 0; y < h; y++) {
+                        dstIn.getPixel(x + dstIn.getMinX(), y + dstIn.getMinY(), dstRgba);
+                        dstRgba[0] = Math.min(255, Math.max(0, dstRgba[0] + color.red()));
+                        dstRgba[1] = Math.min(255, Math.max(0, dstRgba[1] + color.green()));
+                        dstRgba[2] = Math.min(255, Math.max(0, dstRgba[2] + color.blue()));
+                        dstRgba[3] = 255;
+                        dstOut.setPixel(x + dstOut.getMinX(), y + dstOut.getMinY(), dstRgba);
+                    }
+                }
+            }
+        }
     }
 }
